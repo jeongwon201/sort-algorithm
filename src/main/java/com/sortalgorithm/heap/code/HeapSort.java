78c224e9a92f175a86d98b75808ecfadea9af4cd
jeongwon <jeongwon201@naver.com>
2023-01-08 18:13:32 +0900

    heap sort update

--- src/main/java/com/sortalgorithm/heap/code/HeapSort.java
+++ src/main/java/com/sortalgorithm/heap/code/HeapSort.java
@@ -18,44 +18,45 @@ public class HeapSort {
         for (int idx = arr.length - 1; idx > 0; idx--) {
 
             // 가장 먼저 최대 힙을 구성한 후 정렬까지 SWAP 과정은 항상 일어난다.
-            int tmp = arr[0];
-            arr[0] = arr[idx];
-            arr[idx] = tmp;
+            swap(arr, 0, idx);
 
             // SWAP 후 가장 마지막 노드를 제외한 나머지 노드를 다시 최대 힙으로 구성한다.
             heapify(arr, 0, idx - 1);
         }
     }
 
-    static void heapify(int[] arr, int parentIdx, int lastIdx) {
-        int leftChildIdx, rightChildIdx, largeValueIdx;
+    static void heapify(int[] arr, int parent, int last) {
+        int left, right, big;
 
-        while ((parentIdx * 2) + 1 <= lastIdx) {
-            leftChildIdx = (parentIdx * 2) + 1;
-            rightChildIdx = (parentIdx * 2) + 2;
-            largeValueIdx = parentIdx;
+        while ((parent * 2) + 1 <= last) {
+            left = (parent * 2) + 1;
+            right = (parent * 2) + 2;
+            big = parent;
 
-            if (arr[leftChildIdx] > arr[largeValueIdx]) {
-                largeValueIdx = leftChildIdx;
+            if (arr[left] > arr[big]) {
+                big = left;
             }
 
             // 오른쪽 노드의 유무를 판단하기 위해 마지막 인덱스와 비교하는 조건이 추가되었다.
-            if (rightChildIdx <= lastIdx && arr[rightChildIdx] > arr[largeValueIdx]) {
-                largeValueIdx = rightChildIdx;
+            if (right <= last && arr[right] > arr[big]) {
+                big = right;
             }
 
-            // 미리 선언해 부모 노드의 인덱스로 초기화해둔 largeValueIdx 이 parentIdx 와 같지 않다면,
+            // 미리 선언해 부모 노드의 인덱스로 초기화해둔 big 이 parent 와 같지 않다면,
             // 왼쪽 또는 오른쪽 자식이 더 크다는 의미이므로, SWAP 수행 후 자식 노드의 서브 트리가
-            // 최대 힙의 조건을 만족하는 지 확인하기 위해, parentIdx를 자식 노드의 인덱스로 변경해준다.
-            if (largeValueIdx != parentIdx) {
-                int tmp = arr[parentIdx];
-                arr[parentIdx] = arr[largeValueIdx];
-                arr[largeValueIdx] = tmp;
-
-                parentIdx = largeValueIdx;
+            // 최대 힙의 조건을 만족하는 지 확인하기 위해, parent를 자식 노드의 인덱스로 변경해준다.
+            if (big != parent) {
+                swap(arr, parent, big);
+                parent = big;
             } else {
                 return;
             }
         }
+    }
+
+    static void swap(int[] arr, int a, int b) {
+        int tmp = arr[a];
+        arr[a] = arr[b];
+        arr[b] = tmp;
     }
 }
