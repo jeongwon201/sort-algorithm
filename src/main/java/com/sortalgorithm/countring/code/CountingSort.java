0e54a87f8737379153812fefd32cd941653002fc
jeongwon <jeongwon201@naver.com>
2023-01-08 18:29:28 +0900

    counting sort update

--- src/main/java/com/sortalgorithm/countring/code/CountingSort.java
+++ src/main/java/com/sortalgorithm/countring/code/CountingSort.java
@@ -39,4 +39,21 @@ class CountingSort {
 
         return sortedNums;
     }
+
+    // 카운팅 정렬 응용하기
+    // 누적합 과정을 생략하고 바로 결과를 출력합니다.
+    static void sort_2(int[] a, int range) {
+        int[] arr = new int[101]; // 수의 범위 : 0 ~ 100
+
+        for (int i = 0; i < 50; i++) {	// 수열의 크기 : 50
+            arr[(int) (Math.random() * 101)]++;	// 0 ~ 100, 계수를 하기 때문에 ++ 로 1씩 증가
+        }
+
+        for(int i = 0; i < arr.length; i++) {
+
+            while(arr[i]-- > 0) {	// arr 값이 0보타 클 경우
+                System.out.print(i + " ");
+            }
+        }
+    }
 }
